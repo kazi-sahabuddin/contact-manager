1b321f964f4bb061a0b5b70b0d610f4e33bcdcd9
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2024-11-25 08:57:17 +0600

    feat: user registration in progress

--- src/main/java/com/sahabuddin/contactmanager/controllers/HomeController.java
+++ src/main/java/com/sahabuddin/contactmanager/controllers/HomeController.java
@@ -1,12 +1,23 @@
 package com.sahabuddin.contactmanager.controllers;
 
+import com.sahabuddin.contactmanager.entities.User;
+import com.sahabuddin.contactmanager.helper.Message;
+import com.sahabuddin.contactmanager.respositories.UserRepository;
+import com.sahabuddin.contactmanager.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserRepository userRepository;
+
     @GetMapping(value = "/home")
     public String home(Model model) {
         model.addAttribute("title", "Home | Contact Manager");
@@ -22,6 +33,36 @@ public class HomeController {
     @GetMapping(value = "/signup")
     public String signup(Model model) {
         model.addAttribute("title", "Sign up | Contact Manager");
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping(value = "/do-register")
+    public String userSignup(@ModelAttribute("user") User user, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session) {
+        try{
+            if (!agreement) {
+                log.info("Agreement not available");
+            }
+            model.addAttribute("title", "Sign up | Contact Manager");
+            user.setRole("ROLE_USER");
+            user.setEnabled(true);
+            user.setImageUrl("default.png");
+            User saved = userRepository.save(user);
+            log.info("User registered successfully {}", saved);
+            log.info("user info: {}", user.toString());
+            log.info("agreement: {}", agreement);
+
+            model.addAttribute("user", new User());
+            session.setAttribute("message", new Message("Registration successfully !", "alert-success"));
+
+
+        } catch (Exception e){
+            model.addAttribute("title", "Sign up | Contact Manager");
+            model.addAttribute("user", new User());
+            model.addAttribute("error", e.getMessage());
+            log.error("error: {}", e.getMessage());
+            session.setAttribute("message", new Message("Something went wrong!"+e.getMessage(), "alert-error"));
+        }
         return "signup";
     }
 }
---
+++ src/main/java/com/sahabuddin/contactmanager/helper/Message.java
@@ -0,0 +1,14 @@
+package com.sahabuddin.contactmanager.helper;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+
+    private String content;
+    private String type;
+}
